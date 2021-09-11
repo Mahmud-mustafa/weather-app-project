0c652a4ce7a4a20cb3c7228a8ce6791e2024d4a4
Mahmud-mustafa <89031146+Mahmud-mustafa@users.noreply.github.com>
2021-09-11 09:28:40 +0200

    date fix

--- website/app.js
+++ website/app.js
@@ -6,7 +6,7 @@ const getId = (id)=>{
 }
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate =d.getDate() +'.'+ (d.getMonth() + 1 ) + '.' + d.getFullYear();
 //HTML DOM element
 button = getId('generate');
 // Event listener to add function to existing HTML DOM element
